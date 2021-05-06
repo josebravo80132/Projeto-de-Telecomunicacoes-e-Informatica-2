33fad2b11f1ed14d92610c81a331ad0349456420
jpmm98 <zejunior98@gmail.com>
2021-05-06 16:15:25 +0100

    Servidor com comunicacao concorrente aos peers completo

--- version/v1.0/Server.py
+++ version/v1.0/Server.py
@@ -1,5 +1,7 @@
 import socket
 from threading import Thread
+import time
+import errno
 
 serverIP = '10.0.0.10' #socket.gethostbyname(socket.gethostname())
 gestorIP = '10.0.0.20'
@@ -22,71 +24,38 @@ def waitGestorRequest():
 			break
 		return gestorRequest 
 
-def activatePeers(request):
-	#|ID| TESTE | IP_Inicial | IP_Final | Opcional |
-	requestFields = request.split()
-	typeID = requestFields[0]
-	teste = requestFields[1]
-	peerA_IP = requestFields[2]
-	peerB_IP = requestFields[3]
-	optional = requestFields[4]
-
-	#|ID| TESTE | IP_Final | Opcional |
-	message = typeID+' '+teste+' '+peerB_IP+' '+optional
-	print("Starting TCP connection with "+peerA_IP+" ...")
-	port = 5000
-
-	socket_peerA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	socket_peerA.connect((peerA_IP, port))
-	socket_peerA.send(message.encode())
-	socket_peerA.close()
-
-	print("Starting TCP connection with "+peerB_IP+" ...")
-	port = 5000
-
-	socket_peerB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	socket_peerB.connect((peerB_IP, port))
-	socket_peerB.send(message.encode())
-	socket_peerB.close()
-
 
 class activatePeers_thread(Thread):
 
 	def __init__(self, peer_socket, message):
 		Thread.__init__(self)
-		self.peer_socket = peer_sockets
+		self.peer_socket = peer_socket
 		self.message = message
 
 	def run(self):
-		while True:
-			peer_socket.send(message.encode())
-			peer_socket.settimeout(10)
+
+		self.peer_socket.sendall(message.encode())
+		(peer_addr, peer_port) = self.peer_socket.getpeername()
+		while peer_addr == peerB_IP:
+			print("Entrou no while")
+			self.peer_socket.settimeout(20)
 			try: 
-				res = peer_socket.recv(1024)
-			except:
-				print("An exception ocurred, timeout?")
-				peer_socket.close()
-				break
-			#sendResult_Gestor(res)
-			print("-Monitorization Result: "+str(res))
-			break
-			
+				res = self.peer_socket.recv(1024).decode()
+			except socket.error as e:
+				err = e.args[0]
+				if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+					sleep(1)
+					print("No data available")
+					continue
+				else:
+					print(e)
+					self.peer_socket.close()
+					break
+			sendResult_Gestor(res)
+			break	
 
-
-def waitPeerResult():
-	port = 5000
-
-	print("*** Server ***\n (TCP) Waiting Peer result on port "+str(port)+" ...")
-	socket_result = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	socket_result.bind((serverIP, port))
-	socket_result.listen(1)
-	connection, address = socket_result.accept()
-	print("Connected from "+str(address))
-	while True:
-		peerResult = connection.recv(1024)
-		if not peerResult:
-			break
-		return peerResult 
+		self.peer_socket.shutdown(socket.SHUT_RDWR)
+		self.peer_socket.close()	
 
 def sendResult_Gestor(resultado):
 	port = 5005
@@ -94,31 +63,48 @@ def sendResult_Gestor(resultado):
 	print("(TCP) Connecting to Gestor "+str(gestorIP)+" ...")
 	socket_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	socket_TCP.connect((gestorIP, port))
-	socket_TCP.send(resultado)
+	socket_TCP.send(resultado.encode())
 	socket_TCP.close()
 
 
 if __name__ == '__main__':
+
+	threads = []
+	
 	while 1:
 		gestorRequest = waitGestorRequest().decode()
 		print("Gestor Request: "+str(gestorRequest))
+
+		socket_peer_inicial = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket_peer_final = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		
+
 		requestFields = gestorRequest.split()
 		typeID = requestFields[0]
 		teste = requestFields[1]
 		peerA_IP = requestFields[2]
 		peerB_IP = requestFields[3]
 		optional = requestFields[4]
+
+		socket_peer_inicial.connect((peerA_IP, TCP_PORT))
+		socket_peer_final.connect((peerB_IP, TCP_PORT))
+		
 		
 		message = typeID+' '+teste+' '+peerB_IP+' '+optional
 
-		socket_peer_inicial = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket_peer_final = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket_peer_inicial.connect((peerA_IP, TCP_PORT))
-		socket_peer_final.connect((peerB_IP, TCP_PORT))
 
 		thread_inicial = activatePeers_thread(socket_peer_inicial, message)
 		thread_final = activatePeers_thread(socket_peer_final, message)
+
+		thread_inicial.start()
+		thread_final.start()
+
+		threads.append(thread_inicial)
+		threads.append(thread_final)
+
+		for t in threads:
+			t.join()
+
 
 		#socket_peer.connect()
 		#activatePeers(gestorRequest)
