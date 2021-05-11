3111c42adb38e53dd592bc640b035dac6f9d7b8a
moutinho <josepedrovilelabravo@gmail.com>
2021-05-11 19:33:36 +0100

    Update Traceroute

--- version/v2.0/Gestor_v1.py
+++ version/v2.0/Gestor_v1.py
@@ -1,6 +1,4 @@
 import socket
-import sqlite3
-import time
 
 
 def switch(number):
@@ -13,16 +11,18 @@ def switch(number):
 	func = switcher.get(number)
 	return func()
 
+
 def getMainMenu():
     print("*** Gestor ***")
     print("1 - Monitorização de rede ")
-    #print("2 - Histórico de testes efetuados")
+    # print("2 - Histórico de testes efetuados")
     print("0 - Sair")
     sel = int(input("Selecione uma opção: "))
     switch(sel)
 
+
 def getMenuTestes():
-    
+
     print(" *** Peers *** ")
     print("* 192.168.1.0 *")
     print("* 192.168.1.0 *")
@@ -46,11 +46,10 @@ def getMenuTestes():
         optional = input("Intervalo de monitorização (s): ")
 
     # ID | Teste | Peer A | Peer B | Opcional/IntervaloMonitorizaçao
-    messageToServer = '0 '+teste_selecionado+' '+peerInicial+' '+peerFinal+' '+optional
+    messageToServer = '0 '+teste_selecionado + \
+        ' '+peerInicial+' '+peerFinal+' '+optional
     connect_to_server(messageToServer)
 
-    return [peerInicial, peerFinal]
-    
 
 def connect_to_server(message):
     serverIP = input("Endereço IP do servidor: ")
@@ -62,8 +61,9 @@ def connect_to_server(message):
     socket_TCP.send(message.encode())
     socket_TCP.close()
 
+
 def waitServerResult():
-    gestorIP = '10.0.0.20' #socket.gethostbyname(socket.gethostname())
+    gestorIP = socket.gethostbyname(socket.gethostname())
     port = 5005
     print("Waiting on port "+str(port)+" ...")
 
@@ -76,34 +76,19 @@ def waitServerResult():
         result = connection.recv(1024)
         if not result:
             break
-        return result 
-	
+        return result
+
+
+def printMonitorizationResult(resultado):
+    # ID TipoTeste Resultado (10.0.18.1),(10.0.2.1),(10.0.1.1),
+    resultFields = resultado.split()
+    print("\n*** Resultado ***\n"+' Tipo de Teste: '+str(resultFields[1])+'\n Resultado: '+str(
+        resultFields[2])+'\n Rota Expectável: '+str(resultFields[3])+'\n')
+
 
 if __name__ == '__main__':
     while 1:
-	    #getMainMenu()
-	    conn = sqlite3.connect("localhost")
-	    print("Garantido acesso a base de dados\n")
-	    print("1 - Menu de Monitorizacao")
-	    print("2 - Aceder a base de dados")
-	    opcao = input("Escolha a opcao: ")
-	    if opcao == "1":
-	    	aux = getMenuTestes()
-	    	server_result = waitServerResult().decode()
-	    	x = server_result.split()
-	    	conn.execute("INSERT INTO TESTES (ID, PEER_I, PEER_F, RESULT) VALUES(?,?,?,?)", (x[1], aux[0], aux[1],  x[2]))
-	    	conn.commit()
-	    	print("Adicionado com sucesso a base de dados")
-	    	print("\n Resultado: "+str(server_result))
-	    if opcao == '2':
-	   		cursor = conn.execute("SELECT SEQ, ID, PEER_I, PEER_F, RESULT, TIMESTAMP from TESTES")
-
-	   		for row in cursor:
-	   			print("SEQ = ", row[0])
-	   			print("ID = ", row[1])
-	   			print("PEER_I = ", row[2])
-	   			print("PEER_F = ", row[3])
-	   			print("RESULT = ", row[4])
-	   			print("TIMESTAMP = ", row[5])
-
-	   		print("Operacao Concluida!")
+	    # getMainMenu()
+        getMenuTestes()
+        server_result = waitServerResult().decode()
+        printMonitorizationResult(server_result)
