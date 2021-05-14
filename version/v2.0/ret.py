b0273480230e3b1b0c7f30ab05196bcf0c2203be
manuelmendes1 <50181070+manuelmendes1@users.noreply.github.com>
2021-05-14 13:20:41 +0100

    Update ret.py

--- version/v2.0/ret.py
+++ version/v2.0/ret.py
@@ -4,253 +4,75 @@ import sys
 import os
 import argparse
 
+def disp():
+	program = '''
 
-
-
-#sudo /etc/init.d/snmpd start
-
-
-#snmpwalk -v 2c localhost -c gr2020 .1.3.6.1.2.1.4.20.1.1 
-
-
-program = '''
-
-x = {}
-y = {} 
-z = {} 
-w = {}
-t = {}
-u = {}
+x, y, z, w, t, u = {}, {}, {}, {}, {}, {}
 
 output = os.popen('/etc/init.d/snmpd start').read()
-print(output)
-
 output = os.popen('snmpwalk -v 2c localhost -c gr2020 .1.3.6.1.2.1.4.20.1.1 ').read()
-print(output)
-
 x = output.split(" ")
-
 output = os.popen('snmpwalk -v 2c localhost -c gr2020 .1.3.6.1.2.1.4.22.1.3 ').read()
-print(output)
-
 y = output.split(" ")
-
 output = os.popen('snmpwalk -v 2c localhost -c gr2020 .1.3.6.1.2.1.1.3 ').read()
-print(output)
-
 z = output.split(" ")
-
 output = os.popen('snmpwalk -v 2c localhost -c gr2020 .1.3.6.1.2.1.1.5 ').read()
-print(output)
-
 w = output.split(" ")
-
 output = os.popen('snmpwalk -v 2c localhost -c gr2020 .1.3.6.1.2.1.7.1 ').read()
-print(output)
-
 t = output.split(" ")
-
 output = os.popen('snmpwalk -v 2c localhost -c gr2020 .1.3.6.1.2.1.7.4 ').read()
-print(output)
-
 u = output.split(" ")
 
+	'''
 
-'''
+	loc = {}
 
-loc = {}
+	exec(program, globals(), loc)
 
-exec(program, globals(), loc)
+	texto, texto_v1, texto_v2, texto_v3 = [] [] [] []
+	str_final = "" 
 
-texto = []
-texto.extend(loc['x'])
+	for i in loc:
 
-texto_v1 = []
-texto_v2 = []
-texto_v3 = []
-texto_final_1 = []
-texto_final_2 = []
-texto_final_3 = []
-texto_final_4 = []
-texto_final_5 = []
-texto_final_6 = []
-texto_final_7 = []
+		if(i=='x'):
+			texto.append("intf:")
+			texto.extend(loc[i])
+		if(i=='y'):
+			texto.append(" ip_rlocal:")
+			texto.extend(loc[i])
+		if(i=='z'):
+			texto.append(" t_act:")
+			texto.extend(loc[i])
+		if(i=='w'):
+			texto.append(" nome:")
+			texto.extend(loc[i])
+		if(i=='t'):
+			texto.append(" udp_rec:")
+			texto.extend(loc[i])
+		if(i=='u'):
+			texto.append(" udp_env:")
+			texto.extend(loc[i])
 
-for i in range(len(texto)):
-	if(texto[i].startswith("I") == False):
-		texto_v1.append(texto[i])
+	for i in range(len(texto)):
+		if("\n" in texto[i] or "intf:" in texto[i] or " ip_rlocal:" in texto[i] or " t_act:" in texto[i] or " nome:" in texto[i] or " udp_rec:" in texto[i] or " udp_env:" in texto[i]):
+			texto_v1.append(texto[i])
+			
+	for i in range(len(texto_v1)):
+		texto_v2.append(texto_v1[i].splitlines())
 		
-for i in range(len(texto_v1)):
-	if(texto_v1[i].endswith("=")==False):
-		texto_v2.append(texto_v1[i])
-		
-for i in range(len(texto_v2)):
-	if "\nIP-MIB::ipAdEntAddr." in texto_v2[i]:
-		texto_v3.append(texto_v2[i].split("\nIP-MIB::ipAdEntAddr."))
+	for i in range(len(texto_v2)):
+		for j in range(len(texto_v2[i])):
+			word = texto_v2[i][j]
+			if(len(word)<=15):
+				texto_v3.append(word)
+				
+	for i in texto_v3: 
+		str_final += i+' '  
 
+	return str_final
 
-for i in range(len(texto_v3)):
-	if(len(texto_v3[i][0]) > 2 ):
-		texto_final_1.append(texto_v3[i][0])
-		
-#hwewebfubweubfuwebfwebfuiwebfiubweifiu
+a = disp()
 
-texto.clear()
-texto_v1.clear()
-texto_v2.clear()
-texto_v3.clear()
-texto.extend(loc['y'])
-
-for i in range(len(texto)):
-	if(texto[i].startswith("I") == False):
-		texto_v1.append(texto[i])
-			
-for i in range(len(texto_v1)):
-	if(texto_v1[i].endswith("=")==False):
-		texto_v2.append(texto_v1[i])
-		
-for i in range(len(texto_v2)):
-	if "\nIP-MIB::ipNetToMediaNetAddress." in texto_v2[i]:
-		texto_v3.append(texto_v2[i].split("\nIP-MIB::ipNetToMediaNetAddress."))
-
-for i in range(len(texto_v3)):
-	for j in range(len(texto_v3[i])):
-		if(len(texto_v3[i][j]) > 0 ):
-			texto_final_2.append(texto_v3[i][j])
-
-#hwewebfubweubfuwebfwebfuiwebfiubweifiu
-
-texto.clear()
-texto_v1.clear()
-texto_v2.clear()
-texto_v3.clear()
-texto.extend(loc['z'])
-
-for i in range(len(texto)):
-	if(texto[i].startswith("I") == False):
-		texto_v1.append(texto[i])
-			
-for i in range(len(texto_v1)):
-	if(texto_v1[i].endswith("\n")== True):
-		texto_v2.append(texto_v1[i])
-
-for i in range(len(texto_v2)):
-	if "\n" in texto_v2[i]:
-		texto_v3.append(texto_v2[i].split("\n"))
-
-for i in range(len(texto_v3)):
-	for j in range(len(texto_v3[i])):
-		if(len(texto_v3[i][j]) > 0 ):
-			texto_final_3.append(texto_v3[i][j])
-			
-#hwewebfubweubfuwebfwebfuiwebfiubweifiu
-
-texto.clear()
-texto_v1.clear()
-texto_v2.clear()
-texto_v3.clear()
-texto.extend(loc['w'])
-
-for i in range(len(texto)):
-	if(texto[i].startswith("I") == False):
-		texto_v1.append(texto[i])
-			
-for i in range(len(texto_v1)):
-	if(texto_v1[i].endswith("\n")== True):
-		texto_v2.append(texto_v1[i])
-
-for i in range(len(texto_v2)):
-	if "\n" in texto_v2[i]:
-		texto_v3.append(texto_v2[i].split("\n"))
-
-for i in range(len(texto_v3)):
-	for j in range(len(texto_v3[i])):
-		if(len(texto_v3[i][j]) > 0 ):
-			texto_final_4.append(texto_v3[i][j])
-
-#hwewebfubweubfuwebfwebfuiwebfiubweifiu
-
-texto.clear()
-texto_v1.clear()
-texto_v2.clear()
-texto_v3.clear()
-texto.extend(loc['t'])
-
-for i in range(len(texto)):
-	if(texto[i].startswith("I") == False):
-		texto_v1.append(texto[i])
-			
-for i in range(len(texto_v1)):
-	if(texto_v1[i].endswith("\n")== True):
-		texto_v2.append(texto_v1[i])
-
-for i in range(len(texto_v2)):
-	if "\n" in texto_v2[i]:
-		texto_v3.append(texto_v2[i].split("\n"))
-
-for i in range(len(texto_v3)):
-	for j in range(len(texto_v3[i])):
-		if(len(texto_v3[i][j]) > 0 ):
-			texto_final_5.append(texto_v3[i][j])
-
-
-
-#hwewebfubweubfuwebfwebfuiwebfiubweifiu
-
-texto.clear()
-texto_v1.clear()
-texto_v2.clear()
-texto_v3.clear()
-texto.extend(loc['u'])
-
-for i in range(len(texto)):
-	if(texto[i].startswith("I") == False):
-		texto_v1.append(texto[i])
-			
-for i in range(len(texto_v1)):
-	if(texto_v1[i].endswith("\n")== True):
-		texto_v2.append(texto_v1[i])
-
-for i in range(len(texto_v2)):
-	if "\n" in texto_v2[i]:
-		texto_v3.append(texto_v2[i].split("\n"))
-
-for i in range(len(texto_v3)):
-	for j in range(len(texto_v3[i])):
-		if(len(texto_v3[i][j]) > 0 ):
-			texto_final_6.append(texto_v3[i][j])
-
-
-print(texto_final_1)
-print(texto_final_2)
-print(texto_final_3)
-print(texto_final_4)
-print(texto_final_5)
-print(texto_final_6)
-
-texto_final_7 = ['interfaces:']
-texto_final_7.extend(texto_final_1)
-texto_final_7.append(" ip_redeslocal:")
-texto_final_7.extend(texto_final_2)
-texto_final_7.append(" tempo_act:")
-texto_final_7.extend(texto_final_3)
-texto_final_7.append(" nome:")
-texto_final_7.extend(texto_final_4)
-texto_final_7.append(" udp_rec:")
-texto_final_7.extend(texto_final_5)
-texto_final_7.append(" udp_env:")
-texto_final_7.extend(texto_final_6)
-
-str_final = ""  
-for i in texto_final_7: 
-	str_final += i  
-
-
-print(str_final)
-
-
-
-
-
+print(a)
 
 
