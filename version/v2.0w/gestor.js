1d13de5f012ce90aad4b4f0745cb10eb630c0a57
José Bravo <51756941+josebravo80132@users.noreply.github.com>
2021-05-18 18:01:02 +0100

    Update gestor.js

--- version/v2.0w/gestor.js
+++ version/v2.0w/gestor.js
@@ -1,4 +1,6 @@
 
+const express = require('express')
+const web = express()
 
 const net = require("net");
 const readline = require("readline").createInterface({
@@ -13,8 +15,34 @@ const options = {
 };
 
 const message = new String('0 1 10.0.16.20 10.0.15.20 1');
+var encodedMessage = Buffer.from(message, 'utf-8');
+	readline.question('Mandar mensagem de latencia?', res => {
+  	console.log(`a preparar!`);
+  	if(res != null){
+		let gestor = net.connect(options, () =>{
+			gestor.write(encodedMessage);
+			console.log("Sent " + message + " to " + options.host);
+		})
 
-let gestor = net.connect(options, () =>{
-	encodeURI(message);
-	console.log("connected!");
+		gestor.on('data', (data) => {
+			console.log("Recebido "+data.toString('utf-8'));
+		})
+
+		web.get('/', (req, res) => {
+			res.send('Hello World!')
+		})	
+	}else{
+		console.log('Nao recebido nada')
+	}
+  readline.close();
+});
+
+
+
+
+
+
+
+web.listen(3000, () => {
+	console.log('Example app listening at gttp:/localhost:3000')
 })
